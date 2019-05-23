3d1850184e00ea13e897a873fb6ef07ff94152d6
Philipp Schlegel <observing@web.de>
2019-05-23 17:04:45 +0100

    bump version 0.0.2 -> 0.1.0

--- navis/__init__.py
+++ navis/__init__.py
@@ -11,7 +11,7 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 
-__version__ = "0.0.2"
+__version__ = "0.1.0"
 
 from . import config
 
