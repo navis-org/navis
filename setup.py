d0c8d80f122863992018c483098b3ebb30f9c25a
Philipp Schlegel <observing@web.de>
2021-01-06 12:35:37 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -16,21 +16,21 @@ with open('requirements.txt') as f:
     requirements = [l for l in requirements if l and not l.startswith('#')]
 
 LONG_DESCRIPTION = """
-NAVis library is a Python 3 library for analysis and visualization of neuron
+NAVis is a Python 3 library for analysis and visualization of neuron
 morphology.
 
 Features include:
 
 * work with various neuron types: skeletons, meshes, dotprops
 * 2D (matplotlib) and 3D (vispy or plotly) plotting
-* virtual neuron surgery (cutting, stitching, pruning, rerooting, ...)
-* analyse morphology (e.g. NBLAST) and connectivity
+* virtual neuron surgery: cutting, stitching, pruning, rerooting, etc
+* analyze morphology (e.g. NBLAST) and connectivity
 * transform neurons between template brains
-* Python bindings for R neuron libraries (e.g. nat, nat.nblast and elmr)
-* load neurons directly from `neuromorpho.org <http://neuromorpho.org>`_ or :ref:`neuPrint<neuprint_intro>`
+* load neurons directly from `neuPrint <https://neuprint.janelia.org>`_ and `neuromorpho.org <http://neuromorpho.org>`_
 * interface with Blender 3D
+* interface with R neuron libraries (e.g. nat, nat.nblast and elmr)
 * import-export from/to SWC
-* extensible - see for example `pymaid <https://pymaid.readthedocs.io/en/latest/>`_
+* designed to be extensible - see for example `pymaid <https://pymaid.readthedocs.io/en/latest/>`_
 
 Check out the `Documentation <http://navis.readthedocs.io/>`_.
 """
@@ -40,14 +40,14 @@ setup(
     version=verstr,
     packages=find_packages(),
     license='GNU GPL V3',
-    description='Neuron Analysis and Visualization Library',
+    description='Neuron Analysis and Visualization library',
     long_description=LONG_DESCRIPTION,
     url='http://navis.readthedocs.io',
     author='Philipp Schlegel',
     author_email='pms70@cam.ac.uk',
-    keywords='Neuron Analysis Visualization Anatomy Connectivity',
+    keywords='Neuron Analysis Visualization Anatomy Connectivity Transform',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
@@ -57,6 +57,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     install_requires=requirements,
     extras_require={'extras': []},
