fcf6f93006e3c179cbed265475f3a3d6fe12270a
Philipp Schlegel <observing@web.de>
2021-04-18 15:37:04 +0100

    bump version 0.5.2 -> 0.5.3

--- navis/__version__.py
+++ navis/__version__.py
@@ -1,2 +1,15 @@
-__version__ = "0.5.2"
-__version_vector__ = (0, 5, 2)
+#    This script is part of navis (http://www.github.com/schlegelp/navis).
+#    Copyright (C) 2018 Philipp Schlegel
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+__version__ = "0.5.3"
+__version_vector__ = (0, 5, 3)
