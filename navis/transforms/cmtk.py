b3b229731335d43bef762c2cd737a23177092560
Philipp Schlegel <observing@web.de>
2021-04-18 15:15:14 +0100

    cmtk: add another potential directory

--- navis/transforms/cmtk.py
+++ navis/transforms/cmtk.py
@@ -32,6 +32,7 @@ __all__ = ['xform_cmtk']
 _search_path = os.environ['PATH']
 _search_path = [i for i in _search_path.split(';') if len(i) > 0]
 _search_path += ['~/bin',
+                 '/usr/lib/cmtk/bin/',
                  '/usr/local/lib/cmtk/bin',
                  '/usr/local/bin',
                  '/opt/local/bin',
