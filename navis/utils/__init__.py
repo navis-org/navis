9b428e756fb331d0d4b506d0928c44fee0556868
Philipp Schlegel <observing@web.de>
2021-04-21 21:50:26 +0100

    first pass at decorator to map functions over NeuronLists

--- navis/utils/__init__.py
+++ navis/utils/__init__.py
@@ -1,7 +1,21 @@
+#    This script is part of navis (http://www.github.com/schlegelp/navis).
+#    Copyright (C) 2018 Philipp Schlegel
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
 from .iterables import make_iterable, make_non_iterable, is_iterable
 from .misc import (is_jupyter, set_loggers, set_pbars, unpack_neurons,
                    set_default_connector_colors, parse_objects,
-                   is_url, make_url, lock_neuron, make_volume, sizeof_fmt)
+                   is_url, make_url, lock_neuron, make_volume, sizeof_fmt,
+                   map_neuronlist)
 from .validate import validate_options, validate_table
 from .eval import (eval_node_ids, eval_neurons, eval_id, eval_conditions,
                    is_mesh, is_numeric, eval_param)
--- navis/utils/misc.py
+++ navis/utils/misc.py
@@ -11,17 +11,20 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 
+import inspect
 import requests
 import urllib
 
 import numpy as np
 import pandas as pd
 
+from collections import defaultdict
 from functools import wraps
 from typing import Optional, Union, List, Iterable, Dict, Tuple, Any
 
 from .. import config, core, transforms
 from .eval import is_mesh
+from .iterables import is_iterable, make_iterable
 
 # Set up logging
 logger = config.logger
@@ -48,6 +51,159 @@ def make_volume(x: Any) -> 'core.Volume':
         return core.Volume(**inits)
 
     raise TypeError(f'Unable to coerce input of type "{type(x)}" to navis.Volume')
+
+
+def map_neuronlist(desc: str = "",
+                   can_zip: List[Union[str, int]] = [],
+                   must_zip: List[Union[str, int]] = [],
+                   progress: bool = True):
+    """Run function on all neurons in the NeuronList.
+
+    Parameters
+    ----------
+    desc :          str
+                    Descriptor to show in the progress bar if run over multiple
+                    neurons.
+    can_zip/
+    must_zip :      list
+                    Names of keyword arguments that need to be zipped together
+                    with the neurons in the neuronlist. For example:
+
+                      some_function(NeuronList([n1, n2, n3]), [p1, p2, p3])
+
+                    Should be executed as:
+
+                      some_function(n1, p1)
+                      some_function(n2, p2)
+                      some_function(n3, p3)
+
+                    `can_zip` will be zipped only if the length matches the
+                    length of the neuronlist. If a `can_zip` argument has only
+                    one value it will be re-used for all neurons.
+
+                    `must_zip` arguments have to have one value for each of the
+                    neurons.
+
+                    Single ``None`` values are always just passed through.
+
+                    Note that for this to consistently work the parameters in
+                    question have to be keyword-only (*).
+    progress :      bool
+                    Whether to show a progress bar or not. Overrruled by
+                    ``config.pbar_hide``.
+
+    To-implement
+    ------------
+
+    allow_parallel : bool
+                    If True and the function is called with `parallel=True`,
+                    will use multiple cores to process the neuronlist.
+
+    """
+    # TODO:
+    # - make can_zip/must_zip work with positional-only argumens to, i.e. let
+    #   it work with integers instead of strings
+    # - in theory, we could also implement parallel processing here
+
+    def decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            nl = None
+            if args:
+                # If there are positional arguments, the first one is thought to
+                # be the input neuron(s)
+                nl = args[0]
+                nl_key = '_args'
+            else:
+                # If not, we have to cross our fingers and hope that there is
+                # either an "x", a "neuron" or a "neurons" keyword argument
+                for key in ('x', 'neuron', 'neurons'):
+                    if key in kwargs:
+                        nl = kwargs[key]
+                        nl_key = key
+                        break
+
+            if isinstance(nl, type(None)):
+                raise ValueError('Unable to identify the neurons for call'
+                                 f'{function}:\n {args}\n {kwargs}')
+
+            # If we have a neuronlist
+            if isinstance(nl, core.NeuronList):
+                # Pop the neurons from kwargs or args so we don't pass the
+                # neurons twice
+                if nl_key == '_args':
+                    args = args[1:]
+                else:
+                    _ = kwargs.pop(nl_key)
+
+                # See if function has inplace parameter
+                sig = inspect.signature(function)
+                has_inplace = 'inplace' in sig.parameters
+                # If function has an inplace parameter
+                if has_inplace:
+                    # See if user has specified inplace, if not use the default
+                    inplace = kwargs.pop('inplace', sig.parameters['inplace'].default)
+                    # If not inplace make a copy (which will be returned)
+                    if not inplace:
+                        nl = nl.copy()
+
+                # Parse "can zip" arguments
+                can_zip_kwargs = [{} for n in nl]
+                for p in can_zip:
+                    # Skip if not present or is None
+                    if p not in kwargs or isinstance(kwargs[p], type(None)):
+                        continue
+
+                    if is_iterable(kwargs[p]):
+                        # If iterable but length does not match complain
+                        le = len(kwargs[p])
+                        if le != len(nl):
+                            raise ValueError(f'Got {le} values of `{p}` for '
+                                             f'{len(nl)} neurons.')
+
+                        # Zip
+                        for i, v in enumerate(kwargs.pop(p)):
+                            can_zip_kwargs[i][p] = v
+
+                # Parse "must zip" arguments
+                must_zip_kwargs = [{} for n in nl]
+                for p in must_zip:
+                    # Skip if not present or is None
+                    if p not in kwargs or isinstance(kwargs[p], type(None)):
+                        continue
+
+                    values = make_iterable(kwargs.pop(p))
+                    if len(values) != len(nl):
+                        raise ValueError(f'Got {len(values)} values of `{p}` for '
+                                         f'{len(nl)} neurons.')
+
+                    # Zip
+                    for i, v in enumerate(values):
+                        must_zip_kwargs[i][p] = v
+
+                # Whether we want to hide the progress bar
+                hide = config.pbar_hide or not progress or len(nl) == 1
+
+                # Whether we need to/can specify inplace=True
+                if has_inplace:
+                    kwargs['inplace'] = True
+
+                # Now run the function for each neuron
+                for i, n in config.tqdm(enumerate(nl),
+                                        desc=desc,
+                                        disable=hide,
+                                        total=len(nl),
+                                        leave=config.pbar_leave):
+
+                    _ = function(n, *args, **kwargs,
+                                 **can_zip_kwargs[i], **must_zip_kwargs[i])
+
+                return nl
+            else:
+                # If single neuron just pass through
+                return function(*args, **kwargs)
+        return wrapper
+    return decorator
 
 
 def lock_neuron(function):
