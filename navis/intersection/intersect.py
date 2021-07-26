1d9830be60d47ddcc0ec0d37a2a8aa4e06efe49c
Philipp Schlegel <observing@web.de>
2021-07-26 09:23:19 +0100

    make in_volume work with MeshNeurons

--- navis/intersection/intersect.py
+++ navis/intersection/intersect.py
@@ -142,7 +142,7 @@ def in_volume(x: Union['core.NeuronObject', Sequence, pd.DataFrame],
 
     Parameters
     ----------
-    x :                 list of tuples | numpy.array | pandas.DataFrame | TreeNeuron | NeuronList
+    x :                 list of tuples | numpy.array | pandas.DataFrame | TreeNeuron | Dotprops | MeshNeuron | NeuronList
 
                         - list/numpy.array is treated as list of x/y/z
                           coordinates. Needs to be shape (N,3): e.g.
@@ -298,15 +298,17 @@ def in_volume(x: Union['core.NeuronObject', Sequence, pd.DataFrame],
             raise
 
     # Make copy if necessary
-    if isinstance(x, (core.NeuronList, core.TreeNeuron, core.Dotprops)):
+    if isinstance(x, (core.NeuronList, core.BaseNeuron)):
         if inplace is False:
             x = x.copy()
 
-    if isinstance(x, (core.TreeNeuron, core.Dotprops)):
+    if isinstance(x, (core.TreeNeuron, core.Dotprops, core.MeshNeuron)):
         if isinstance(x, core.TreeNeuron):
             data = x.nodes[['x', 'y', 'z']].values
         elif isinstance(x, core.Dotprops):
             data = x.points
+        elif isinstance(x, core.MeshNeuron):
+            data = x.vertices
 
         in_v = in_volume(data,
                          vol,
@@ -324,6 +326,11 @@ def in_volume(x: Union['core.NeuronObject', Sequence, pd.DataFrame],
             if isinstance(x, core.TreeNeuron):
                 _ = graph.subset_neuron(x,
                                         subset=x.nodes[in_v].node_id.values,
+                                        inplace=True,
+                                        prevent_fragments=prevent_fragments)
+            elif isinstance(x, core.MeshNeuron):
+                _ = graph.subset_neuron(x,
+                                        subset=in_v,
                                         inplace=True,
                                         prevent_fragments=prevent_fragments)
             elif isinstance(x, core.Dotprops):
