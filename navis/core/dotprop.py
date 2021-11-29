f84d02bfcf432c6732fff0080cbd1ecee01ae128
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2021-11-29 16:25:34 +0000

    Dotprops.dist_dots type annotation fix

--- navis/core/dotprop.py
+++ navis/core/dotprop.py
@@ -332,7 +332,9 @@ class Dotprops(BaseNeuron):
                   other: 'Dotprops',
                   alpha: bool = False,
                   distance_upper_bound: Optional[float] = None,
-                  **kwargs) -> Tuple[np.ndarray, np.ndarray]:
+                  **kwargs) -> Union[
+                      Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray, np.ndarray]
+                    ]:
         """Query this Dotprops against another.
 
         This function is mainly for ``navis.nblast``.
