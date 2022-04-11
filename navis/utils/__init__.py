bb569c8ad43c22fa4b747a6b0412bdfff7fd3af4
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2022-04-11 12:53:18 +0100

    Add user-facing test that vispy works

--- navis/utils/__init__.py
+++ navis/utils/__init__.py
@@ -16,7 +16,7 @@ from .misc import (is_jupyter, set_loggers, set_pbars, unpack_neurons,
                    set_default_connector_colors, parse_objects,
                    is_url, make_url, lock_neuron, make_volume, sizeof_fmt,
                    map_neuronlist, map_neuronlist_df, round_smart,
-                   meshneuron_skeleton, is_blender)
+                   meshneuron_skeleton, is_blender, check_vispy)
 from .validate import validate_options, validate_table
 from .eval import (eval_node_ids, eval_neurons, eval_id, eval_conditions,
                    is_mesh, is_numeric, eval_param)
