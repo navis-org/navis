d52e28fa928e0d631b4713eb0de82e73a06de5cd
Philipp Schlegel <observing@web.de>
2022-12-21 10:14:16 +0100

    navis.plot1d: allow custom palettes/coloring schemes

--- navis/plotting/d.py
+++ navis/plotting/d.py
@@ -23,6 +23,7 @@ import numpy as np
 from typing import Optional, Union, Dict, Tuple, Any
 
 from .. import core, config, graph
+from .colors import prepare_colormap, vertex_colors
 
 logger = config.get_logger(__name__)
 
@@ -40,6 +41,8 @@ def plot1d(x: 'core.NeuronObject',
                                  Dict[Any, colortype],
                                  ]
                            ] = None,
+           color_by: Optional[Union[str, np.ndarray]] = None,
+           palette: Optional[str] = None,
            **kwargs) -> mpl.axes.Axes:
     """Plot neuron topology in 1D according to Cuntz et al. (2010).
 
@@ -53,8 +56,17 @@ def plot1d(x: 'core.NeuronObject',
     x :         TreeNeuron | NeuronList
                 Neuron(s) to plot.
     ax :        matplotlib.ax, optional
-    cmap :      tuple | dict
+    color :     tuple | dict
                 Color. If dict must map neuron UUID to color.
+    palette :   str | array | list of arrays, default=None
+                Name of a matplotlib or seaborn palette. If ``color`` is
+                not specified will pick colors from this palette.
+    color_by :  str | array | list of arrays, default = None
+                Can be the name of a column in the node table of
+                ``TreeNeurons`` or an array of (numerical or categorical)
+                values for each node. Numerical values will be normalized.
+                You can control the normalization by passing a ``vmin``
+                and/or ``vmax`` parameter.
     **kwargs
                 Will be passed to ``matplotlib.patches.Rectangle``.
 
@@ -89,11 +101,32 @@ def plot1d(x: 'core.NeuronObject',
     else:
         raise TypeError(f'Unable plot1d data of type "{type(x)}"')
 
-    if isinstance(color, type(None)):
+    if isinstance(color, type(None)) and isinstance(palette, type(None)):
         color = (0.56, 0.86, 0.34)
 
+    color, _ =  prepare_colormap(color,
+                                 neurons=x,
+                                 palette=palette,
+                                 color_range=1)
+
+    if not isinstance(color_by, type(None)):
+        if not palette:
+            raise ValueError('Must provide `palette` (e.g. "viridis") argument '
+                             'if using `color_by`')
+
+        vertex_map = vertex_colors(x,
+                                   by=color_by,
+                                   use_alpha=False,
+                                   palette=palette,
+                                   vmin=kwargs.pop('vmin', None),
+                                   vmax=kwargs.pop('vmax', None),
+                                   na=kwargs.pop('na', 'raise'),
+                                   color_range=1)
+    else:
+        vertex_map = None
+
     if not ax:
-        fig, ax = plt.subplots(figsize=(8, len(x) / 3))
+        fig, ax = plt.subplots(figsize=(8, len(x)))
         # Make background transparent (nicer for dark themes)
         fig.patch.set_alpha(0)
         ax.patch.set_alpha(0)
@@ -110,20 +143,24 @@ def plot1d(x: 'core.NeuronObject',
         if isinstance(color, dict):
             this_c = color[n.id]
         else:
-            this_c = color
+            this_c = color[ix]
 
         # Get topological sort (root -> terminals)
-        topology = graph.node_label_sorting(n)
+        topology = graph.node_label_sorting(n, weighted=True)
 
         # Get terminals and branch points
+        roots = n.nodes[n.nodes.type == 'root'].node_id.values
         bp = n.nodes[n.nodes.type == 'branch'].node_id.values
         term = n.nodes[n.nodes.type == 'end'].node_id.values
+        breaks = np.concatenate((bp, term, roots))
 
-        # Order this neuron's segments by topology
-        breaks = [topology[0]] + \
-            [n for i, n in enumerate(topology) if n in bp or n in term]
-        segs = [([s for s in n.small_segments if s[0] == end][0][-1], end)
-                for end in breaks[1:]]
+        # Order this neuron's segments by topology (remember that segments are
+        # sorted child -> parent, i.e. distal to proximal)
+        topo_ix = dict(zip(topology, range(len(topology))))
+        segs = sorted(n.small_segments, key=lambda x: topo_ix[x[0]])
+
+        # Keep only the first and the last node in each segment
+        segs = [[s[0], s[1]] for s in segs]
 
         # Now get distances for each segment
         if 'nodes_geodesic_distance_matrix' in n.__dict__:
@@ -133,20 +170,28 @@ def plot1d(x: 'core.NeuronObject',
             # If not, compute matrix for subset of nodes
             dist_mat = graph.geodesic_matrix(n, from_=breaks, directed=False)
 
-        dist = np.array([dist_mat.loc[s[0], s[1]] for s in segs]) / 1000
-        max_x.append(sum(dist))
+        # Get length of each segment
+        lengths = np.array([dist_mat.loc[s[0], s[1]] for s in segs])
+        max_x.append(sum(lengths))
 
         # Plot
         curr_dist = 0
-        for k, d in enumerate(dist):
-            if segs[k][1] in term:
-                c = tuple(np.array(this_c) / 2)
-            else:
-                c = color
+        id2ix = dict(zip(n.nodes.node_id.values, range(n.n_nodes)))
+        for k, l in enumerate(lengths):
+            if isinstance(vertex_map, type(None)):
+                c = this_c
 
-            p = mpatches.Rectangle((curr_dist, ix), d, 1, fc=c, **kwargs)
+                if segs[k][0] in term:
+                    c = tuple(np.array(c) / 2)
+            else:
+                # Get this segments vertex colors
+                node_ix = [id2ix[i] for i in segs[k]]
+                vc = vertex_map[ix][node_ix]
+                c = vc[-1]
+
+            p = mpatches.Rectangle((curr_dist, ix), l, 1, fc=c, **kwargs)
             ax.add_patch(p)
-            curr_dist += d
+            curr_dist += l
 
     ax.set_xlim(0, max(max_x))
     ax.set_ylim(0, len(x))
@@ -154,7 +199,8 @@ def plot1d(x: 'core.NeuronObject',
     ax.set_yticks(np.array(range(0, len(x))) + .5)
     ax.set_yticklabels(x.name)
 
-    ax.set_xlabel('distance [um]')
+    dstring = 'distance'
+    ax.set_xlabel(dstring)
 
     ax.set_frame_on(False)
 
