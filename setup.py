d51995717b8b7f9cba7fa9373ef3e1a1582e4090
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2023-11-14 14:50:15 +0000

    Add py311, py312

--- setup.py
+++ setup.py
@@ -68,12 +68,14 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     install_requires=install_requires,
     extras_require=dict(extras_require),
     tests_require=extras_require["dev"],
     # CI runs against >=3.8
-    python_requires='>=3.9',
+    python_requires='>=3.9,<4.0',
     zip_safe=False,
 
     include_package_data=True
