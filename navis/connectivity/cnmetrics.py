nbl/matching: fix nblast_smart mask indexing, activity_ratio formula, score-threshold matching, bipartite rematch sentinel, empty synapse similarity

navis/connectivity/cnmetrics.py
@@ -111,7 +111,7 @@ def connectivity_sparseness(x: Union[pd.DataFrame, np.ndarray],
     elif which == 'LTS':
         return 1 / (1 - (1 / N)) * (1 - np.nansum(x / N, axis=0) ** 2 / np.nansum(x**2 / N, axis=0))
     elif which == 'activity_ratio':
-        a = (np.nansum(x, axis=0) / N) ** 2 / (np.nansum(x, axis=0) ** 2 / N)
+        a = (np.nansum(x, axis=0) / N) ** 2 / (np.nansum(x**2, axis=0) / N)
         return 1 - a
     else:
         raise ValueError('Parameter "which" must be either "LTS", "LTK" or '

navis/matching/bipartite.py
@@ -196,7 +196,7 @@ def bipartite_match(scores: pd.DataFrame,
             left_ix, right_ix = scipy.optimize.linear_sum_assignment(scores_final, maximize=True)
 
             # Drop matches that have effectively minus infinity scores
-            is_above = scores_final.values[left_ix, right_ix] > 1e-10
+            is_above = scores_final.values[left_ix, right_ix] > -1e10
             left_ix, right_ix = left_ix[is_above], right_ix[is_above]
 
             # Convert to row/column names
@@ -208,7 +208,7 @@ def bipartite_match(scores: pd.DataFrame,
             missing = np.array(list(set(scores_final.columns) - set(right_id)))
 
             # We can only rematch neurons that have a possible match to begin with
-            missing = missing[scores_final[missing].max(axis=0) > 1e-10]
+            missing = missing[scores_final[missing].max(axis=0) > -1e10]
 
             # Transpose matrix
             scores_final = scores_final.T

navis/nbl/backends/builtin.py
@@ -356,11 +356,14 @@ class BuiltinBackend(NblastBackend):
             mask = scr >= sel.reshape(-1, 1)
         else:
             srt = np.argsort(scr.values, axis=1)[:, ::-1]
-            mask = pd.DataFrame(np.zeros(scr.shape, dtype=bool),
-                                columns=scr.columns, index=scr.index)
-            _ = np.arange(mask.shape[0])
+            # Build the mask with numpy fancy indexing (pointwise). Using
+            # `DataFrame.iloc[rows, cols]` here would do cross-product (block)
+            # indexing and mark far too many cells.
+            mask_arr = np.zeros(scr.shape, dtype=bool)
+            _ = np.arange(mask_arr.shape[0])
             for N in range(t):
-                mask.iloc[_, srt[:, N]] = True
+                mask_arr[_, srt[:, N]] = True
+            mask = pd.DataFrame(mask_arr, columns=scr.columns, index=scr.index)
 
         # --- Full NBLAST on the selected pairs --- #
         query_self_hits = np.array([nb.calc_self_hit(n) for n in query_dps])